361c7f0cb1d6a2eb304db3b62240bab93f550fa6
OllieJC <oliver.chalk@digital.cabinet-office.gov.uk>
2021-09-17 17:31:44 +0100

    S3 backend tfstate

--- terraform/aws.tf
+++ terraform/aws.tf
@@ -6,3 +6,11 @@ provider "aws" {
   alias  = "us_east_1"
   region = "us-east-1"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "co-cop-twentysix-edge-tfstate"
+    key    = "edge.tfstate"
+    region = "eu-west-2"
+  }
+}
