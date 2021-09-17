ef899c86ea5383180a6bfb3d1ab025e51acd2681
OllieJC <oliver.chalk@digital.cabinet-office.gov.uk>
2021-09-17 17:53:50 +0100

    Drop requests to CloudFront not using expected domains

--- origin_request/src/origin_request.js
+++ origin_request/src/origin_request.js
@@ -2,6 +2,31 @@
 
 exports.handler = (event, context, callback) => {
     const request = event.Records[0].cf.request;
+    
+    const hosts = [
+      "www.ukcop26.org",
+      "ukcop26.org",
+      "together-for-our-planet.ukcop26.org"
+    ];
+
+    if (hosts.indexOf(request.headers["host"].value) == -1) {
+      callback(null, {
+        status: '404',
+        statusDescription: 'Not Found',
+        headers: {
+            'cache-control': [{
+                key: 'Cache-Control',
+                value: 'max-age=31536000'
+            }],
+            'content-type': [{
+                key: 'Content-Type',
+                value: 'text/html'
+            }]
+        },
+        body: "404",
+      });
+      return;
+    }
 
     if (request.uri.match(/^(\/.well[-_]known)?\/security\.txt$/)) {
       const sectxt = 'https://vdp.cabinetoffice.gov.uk/.well-known/security.txt';
