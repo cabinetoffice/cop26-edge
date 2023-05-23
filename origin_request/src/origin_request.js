d85406b3e273e651eeaa920b4b826a8626565e3c
OllieJC <5426038+OllieJC@users.noreply.github.com>
2023-05-23 12:48:59 +0100

    Add wp-cron to default redirect

--- origin_request/src/origin_request.js
+++ origin_request/src/origin_request.js
@@ -70,7 +70,7 @@ exports.handler = (event, context, callback) => {
     host = host.replace(/^\s*?(www|staging)\./, '').trim();
 
     var uri_normalised = request.uri.replace(/^[\.\/\\]+\//, '');
-    if (uri_normalised.match(/^\/(wp-admin|wp-config.php|wp-login|\.)/)) {
+    if (uri_normalised.match(/^\/(wp-admin|wp-config.php|wp-login|wp-cron|\.)/)) {
       uri_normalised = '/';
     }
 
